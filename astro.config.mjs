6a6e1800b6ed63464f70fef927a58db372c1896e
Zuka Abzianidze <zuka.abzianidze123@gmail.com>
2026-03-19 20:36:26 +0400

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import astroI18next from 'astro-i18next';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+    site: 'https://lovepadel.ge',
     output: 'server',
     adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [astroI18next()],
+    integrations: [astroI18next(), sitemap()],
 });
